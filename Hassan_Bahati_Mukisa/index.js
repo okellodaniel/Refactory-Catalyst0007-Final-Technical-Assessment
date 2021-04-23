21fdc9908301219637d26175cf21bbbce5b7ce4b
HassanBahati <mukisabahati@gmail.com>
2021-04-23 11:08:53 +0300

    added packages

--- Hassan_Bahati_Mukisa/index.js
+++ Hassan_Bahati_Mukisa/index.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
+
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send('hassan Bahati mukisa ')
+})
+
+app.listen(5000, ()=>{
+    console.log('Listening on port 5000')
+})
