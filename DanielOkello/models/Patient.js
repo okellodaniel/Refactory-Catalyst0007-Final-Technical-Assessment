45a02e502b70ffa4382044484a676ce2978183b2
Daniel Okello <o.daniel@student.ciu.ac.ug>
2021-04-23 11:20:12 +0300

    Created my routes, Schema, MVC structure, and created a folder wihtin the cloned Repository

---
+++ DanielOkello/models/Patient.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const patientSchema = new mongoose.Schema({
+    surName:{
+        type:String
+    },
+    givenName:{
+        type:String
+    },
+    dOB:{
+        type:Date
+    },
+    residence:{
+        type:Sting
+    },
+    occupation:{
+        type:String
+    },
+    nationality:{
+        type:String
+    },
+    gender:{
+        type:String
+    },
+    category:{
+        type:String
+    }
+
+
+
+});
+
+// Associating the patient schema to the actual collection Name
+
+module.exports = mongoose.model('Patient', patientSchema);
