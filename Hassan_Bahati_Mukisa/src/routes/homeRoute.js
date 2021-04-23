621e4728dbe46d318e2360f491756fcffdaeaac6
HassanBahati <mukisabahati@gmail.com>
2021-04-23 11:54:59 +0300

    add home.pug

--- Hassan_Bahati_Mukisa/src/routes/homeRoute.js
+++ Hassan_Bahati_Mukisa/src/routes/homeRoute.js
@@ -1,11 +1,25 @@
-//imports 
+//Require express
 const express = require('express');
+const Covid = require('../models/Covid')
 
-//initialsing express router 
+//Create a router 
 const router = express.Router();
 
-//route to display the home route 
-app.get('/', (req, res)=>{
-    res.send('hassan Bahati mukisa ')
-});
+//Render the form when user hits end point /
+router.get('/', (req,res) =>{
+    res.render('home');
+})
 
+router.post('/', async (req,res) => {
+    //Create a promise using try and catch blocks
+    try{ const person = new Covid (req.body)
+        await person.save();
+        res.redirect('/covid/')
+    }
+    catch(err){
+        console.log(err)
+        res.send('Error Posting Data')
+    }
+})
+
+module.exports = router
