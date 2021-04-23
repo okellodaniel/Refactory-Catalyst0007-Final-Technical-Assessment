dc0ce7617b0d30f470bfad63c1b168911a2b55d2
dinton harold <dintonharold@gmail.com>
2021-04-23 12:58:25 +0300

    Created a pug file for storing the registration details as well as adding some routes

--- dintonAinemukamaHarold/routes/covidRegRoutes.js
+++ dintonAinemukamaHarold/routes/covidRegRoutes.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const router = express.Router();
-const registration = require("../models/covidReg");
+const Registration = require("../models/covidReg");
 
 router.get("/", (req, res) => {
     
       res.render("covidReg", {title: "Covid Registration Form" });
   
   });
+  router.post('/', async (req, res) => {
+    try {
+       var newRegistration = new Registration(req.body)
+        await newRegistration.save()
+        console.log(req.body)
+        res.redirect('/')
+    } catch (error) {
+       console.log(error);
+        res.status(404).send('unable to send to the database')
+    }
+ }
+)
 
-
+router.get('/patientDetails', async(req, res) => {
+    var userlist = await Registration.find()
+    res.render('patients', {patients:userlist})
+})
 
 
 
